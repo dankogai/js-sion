da39e5a986da574c23eb886c1e6c24329f3a067f
Dan Kogai <dankogai+github@gmail.com>
2020-08-08 11:02:33 +0900

    add *.ts

---
+++ sion.d.ts
@@ -0,0 +1,16 @@
+export declare const version = "1.0.1";
+export declare const RE_HEXFLOAT: RegExp;
+export declare const RE_HEXFLOAT_G: RegExp;
+export declare const parseHexFloat: (str: string) => number;
+export declare const toHexString: (num: number) => string;
+export declare const stringify: (obj: any, replacer?: (any: any) => any, space?: (number | string), depth?: number) => any;
+export declare const parse: (str: string) => any;
+export declare const SION: {
+    version: string;
+    RE_HEXFLOAT: RegExp;
+    RE_HEXFLOAT_G: RegExp;
+    parseHexFloat: (str: string) => number;
+    toHexString: (num: number) => string;
+    stringify: (obj: any, replacer?: (any: any) => any, space?: (number | string), depth?: number) => any;
+    parse: (str: string) => any;
+};
